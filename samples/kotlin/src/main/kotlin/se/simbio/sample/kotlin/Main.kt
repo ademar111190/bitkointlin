5c7b148f8ccd75c12155efb1d593448b2fa306a8
Ademar Alves de Oliveira <ademar111190@gmail.com>
2016-02-09 23:47:06 -0200

    Add a Kotlin sample of how use bitkointlin

---
+++ samples/kotlin/src/main/kotlin/se/simbio/sample/kotlin/Main.kt
@@ -0,0 +1,44 @@
+package se.simbio.sample.kotlin
+
+import se.simbio.bitkointlin.Bitkointlin
+import se.simbio.bitkointlin.http.fuel.HttpClientImplFuel
+
+/**
+ * You need to setup your bitcoin environment and set below the correct data
+ * @see https://bitcoin.org/en/developer-guide
+ */
+val bitkointlin = Bitkointlin(
+        user = "your_user",
+        password = "your_password",
+        httpAddress = "http://127.0.0.1:8332/",
+        httpClient = HttpClientImplFuel())
+
+fun main(args: Array<String>) {
+    getInfo {
+        getBalance {
+            println("Done")
+        }
+    }
+}
+
+fun getInfo(callback: () -> Unit) {
+    println("getInfo() called")
+    bitkointlin.getInfo({ info ->
+        println("getInfo success, info: $info")
+        callback()
+    }, { error ->
+        println("getInfo error: $error")
+        callback()
+    })
+}
+
+fun getBalance(callback: () -> Unit) {
+    println("getBalance() called")
+    bitkointlin.getBalance({ balance ->
+        println("getBalance success, balance: $balance")
+        callback()
+    }, { error ->
+        println("getBalance error: $error")
+        callback()
+    })
+}
